7966dd2b22e9620fa949e315d1fa44111390325f
josiasveras <josias_veras@hotmail.com>
2021-12-15 19:21:01 -0300

    Update classe Pessoa, input de dados e instanciação e criação de objeto Pessoa

--- src/br/com/generation/aplicacao/Programa.java
+++ src/br/com/generation/aplicacao/Programa.java
@@ -1,12 +1,32 @@
 package br.com.generation.aplicacao;
 
+import java.util.Scanner;
+
 import br.com.generation.dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa("Paulo", 30);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite a sua renda total: ");
+		double renda = sc.nextDouble();
+		
+		System.out.print("Informe o total de seus gastos fixos mensais: ");
+		System.out.print("Considere: água, luz, telefone/internet, aluguel/financiamento, alimentação");
+		double gastosFixos = sc.nextDouble();
+		
+		//Instanciando a classe Pessoa e criando o objeto
+		Pessoa p1 = new Pessoa("Armando", 26, renda, gastosFixos);
+		
+		//System.out.printf("Seu nome é: %s. Sua idade é: %d. Sua renda é: %.2f. Seus gastos são: %.2f", p1.getNome(), p1.getIdade(), p1.getRenda(), p1.getGastosFixos());
+		
+		// Calculo da porcentagem de gastos pela renda
+		double porcenGastos = gastosFixos / renda;
+		
+		System.out.println("Renda: " + porcenGastos);
+		
 	}
 
 }
