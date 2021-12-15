54072c30718cb7f8420b8b862379ec44679bd874
josiasveras <josias_veras@hotmail.com>
2021-12-15 20:21:19 -0300

    Update diversos classe Pessoa e criação de objetos para o filtro de dados

--- src/br/com/generation/aplicacao/Programa.java
+++ src/br/com/generation/aplicacao/Programa.java
@@ -1,5 +1,6 @@
 package br.com.generation.aplicacao;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 import br.com.generation.dominio.Pessoa;
@@ -8,6 +9,9 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
+		double salarioMinimo = 1100.00;
+		
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Digite a sua renda total: ");
@@ -18,15 +22,39 @@ public class Programa {
 		double gastosFixos = sc.nextDouble();
 		
 		//Instanciando a classe Pessoa e criando o objeto
-		Pessoa p1 = new Pessoa("Armando", 26, renda, gastosFixos);
+		//Pessoa pessoaApta = new Pessoa("Armando", 26, 20000.00, 500.00);
+		//Pessoa pessoaInaptaAMenorUmSaleMeio = new Pessoa("Paulo", 26, 1000.00, 700.00);
+		//Pessoa pessoaInaptaBMaior5SalMin = new Pessoa("Maria", 40, 20000.00, 15000.00);
+		//Pessoa pessoaInaptaCSalMinMaior1emeioEMenor60PorCent = new Pessoa("João", 50, 1700.00, 900.00);
+		Pessoa pessoaInaptaDGastoMaior60ERendaEntreUmEMeioECincoSalMin = new Pessoa("Lucas", 48, 2200.00, 1800.00);
 		
 		//System.out.printf("Seu nome é: %s. Sua idade é: %d. Sua renda é: %.2f. Seus gastos são: %.2f", p1.getNome(), p1.getIdade(), p1.getRenda(), p1.getGastosFixos());
 		
 		// Calculo da porcentagem de gastos pela renda
 		double porcenGastos = gastosFixos / renda;
 		
-		System.out.println("Renda: " + porcenGastos);
+		//System.out.println("Renda: " + porcenGastos);
 		
+		if (porcenGastos <= 0.50) {
+			System.out.println("Pretende poupar");
+		} else {
+			if (porcenGastos <= 0.60) {
+				if (renda <= (salarioMinimo * 1.5)) {
+					System.out.println("Boas práticas para poupar <= 1.5 salario Mínimo");
+				} else {
+					System.out.println("Boas práticas para poupar > 1.5 salario Mínimo e <= 0.60");
+				}
+			} else {
+				if (renda <= (salarioMinimo * 1.5)) {
+					System.out.println("Boas práticas para poupar < 1.5 salario Mínimo");
+				} else {
+					if(renda > (salarioMinimo * 5)) {
+						System.out.println("Boas práticas para poupar > 5 salario Mínimo");
+					} else {
+						System.out.println("Boas práticas para poupar gastos > 60%. Renda Entre 1.5 Salário Min e 5 Salário Min");
+					}
+				}
+			}
+		}
 	}
-
 }
