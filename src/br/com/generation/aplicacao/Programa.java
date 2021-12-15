4dab17aaa7569ba2d76cde0a13094708566a1e6a
josiasveras <josias_veras@hotmail.com>
2021-12-15 18:08:23 -0300

    Criação classe Pessoa

--- src/br/com/generation/aplicacao/Programa.java
+++ src/br/com/generation/aplicacao/Programa.java
@@ -1,10 +1,12 @@
 package br.com.generation.aplicacao;
 
+import br.com.generation.dominio.Pessoa;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		
+		Pessoa p1 = new Pessoa("Paulo", 30);
 	}
 
 }
